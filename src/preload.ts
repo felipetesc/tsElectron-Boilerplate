8152d06c2647fc6622ea938665fe50c77adf3874
Felipe Torresini <2336812+felipetesc@users.noreply.github.com>
2020-08-08 19:59:33 -0300

    Added files to project

---
+++ src/preload.ts
@@ -0,0 +1,15 @@
+///<reference path="../node_modules/typescript/lib/lib.dom.d.ts" />
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector: string, text: string) => {
+    const element = document.getElementById(selector);
+    if (element) {
+      element.innerText = text;
+    }
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, (process.versions as any)[type]);
+  }
+});
